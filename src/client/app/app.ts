1aa07d800723d2a4061c9c18d4a181cc46341392
burakakkor <burakakkor@gmail.com>
2017-02-05 17:38:01 +0000

    coveralls integration

--- src/client/app/app.ts
+++ src/client/app/app.ts
@@ -1,4 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import {enableProdMode} from '@angular/core'; //for production
 import { AppModule } from './app.module';
 
+//enableProdMode(); //for production
 platformBrowserDynamic().bootstrapModule(AppModule);
